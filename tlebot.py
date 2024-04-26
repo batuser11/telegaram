d6580e07f3e7c4d23849ee7439e06f18d968a576
batuser11 <167933883+batuser11@users.noreply.github.com>
2024-04-26 03:23:01 +0300

    Update tlebot.py

--- tlebot.py
+++ tlebot.py
@@ -5,7 +5,7 @@ BOT_TOKEN = config('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-@bot.message_handler(commads=["hello", "help"])
+@bot.message_handler(commads=["start","help"])
 def welcome(msg):
     bot.send_message(msg.chat.id, "hello to my bot what you whant?")
     
