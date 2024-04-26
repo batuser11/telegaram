ab39b0b40e21edb0f506b1dcbaf7d51329c26cb3
batuser11 <167933883+batuser11@users.noreply.github.com>
2024-04-26 03:22:02 +0300

    Add files via upload

---
+++ tlebot.py
@@ -0,0 +1,12 @@
+import telebot
+from decouple import config
+
+BOT_TOKEN = config('BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commads=["hello", "help"])
+def welcome(msg):
+    bot.send_message(msg.chat.id, "hello to my bot what you whant?")
+    
+bot.polling()
